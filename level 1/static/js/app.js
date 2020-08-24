2a0bcc2c7d45ecc492152e429c71380b4e0b2d96
olivesun1213 <63970195+olivesun1213@users.noreply.github.com>
2020-08-24 19:32:35 -0400

    updates

--- level 1/static/js/app.js
+++ level 1/static/js/app.js
@@ -1,56 +1,31 @@
 
-//create variables to find references to tbody, input field and serach button.
-var $tbody = document.querySelector("tbody");
-// from index.html id=datetime, id = "filter-btn"
-var $dateInput=document.querySelector("#datetime");
-var $searchButton = document.querySelector("#filter-btn");
-
-// add event listener to search button when button is clicked
-$searchButton.addEventListener("click", handleSearchButtonClick);
-
 // from data.js
 var tableData = data;
 
-//render table 
-function renderTable() {
-	// clear the previous table
-	$tbody.innerHTML = "";
+// get input date and filter button
+var dateTime=d3.select("#datetime");
+var filterBtn=d3.select("#filter-btn")
 
-	// loop through  tabledata
-	for (var i = 0; i < tableData.length; i++) {
-        //create variable get tableData object and its filed
-		var currentSighting = tableData[i];
-		var fields = Object.keys(currentSighting);
-		// create a new row in tbody and set index=i
-		var $row = $tbody.insertRow(i);
+//search by date
+dateTime.on("change",function(){
+	var inputDate=d3.event.target.value;
+	//match date
+	var matchedDate=tableData.filter(info => info.datetime==inputDate);
 
-		// loop through fields and loads data into table
-		for (var j = 0; j < fields.length; j++) {
-            // create a new cell and set text to be the current value in currentSighting for every field
-            var field = fields[j];
-			var $cell = $row.insertCell(j);
-			$cell.innerText = currentSighting[field];
-		}
-	}
-}
+	//render table
+	var tbody=d3.select("tbody");
+	//clear previous table ino
+	tbody.html("");
+	matchedDate.forEach(element=>{
+		//add to table
+		var row=tbody.append("tr");
+		row.append("td").text(element.datetime);
+		row.append("td").text(element.city);
+		row.append("td").text(element.country);
+		row.append("td").text(element.state);
+		row.append("td").text(element.shape);
+		row.append("td").text(element.durationMinutes);
+		row.append("td").text(element.comments);
 
-function handleSearchButtonClick() {
-	 // Format the user's search by removing leading and trailing whitespace, lowercase the string
-	 var inputValue = $dateInput.value.trim().toLowerCase();
-		// when user input data, check to see if the input data matches date data in existing data
-		if (inputValue.length != 0) {
-			tableData = data.filter(function(currentSighting){
-				var matchedDate = currentSighting.datetime;
-				return matchedDate === inputValue;
-	});
-}
-// no input, do nothing
-		else {
-			tableData = data;
-}
-renderTable();
-}
-
-
-// render
-renderTable();
+	})
+})
